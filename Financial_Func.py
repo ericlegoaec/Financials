d65c8ed7d924c0ccecb9a02968a349f5d565793b
Gerard Mazi <gerard.mazi@gmail.com>
2020-10-30 07:26:48 -0700

    Initial

---
+++ Financial_Func.py
@@ -0,0 +1,25 @@
+"""
+FUNCTIONS
+
+@author:    Gerard Mazi
+@date:      2020-10-30
+@email:     gerard.mazi@gmail.com
+@phone:     862-221-2477
+
+"""
+
+import pandas as pd
+import numpy as np
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
+import re
+import matplotlib.pyplot as plt
+
+#roles = pd.read_pickle('store_roles.pkl')
+
+'=============================================================='
+userid = ''
+password = ''
+
+def func():
