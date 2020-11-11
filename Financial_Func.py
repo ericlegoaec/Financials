25652036c1f7cc24e3f2bb03016c7a1df72ba37d
Gerard Mazi <gerard.mazi@gmail.com>
2020-11-11 15:39:28 -0800

    Looped

--- Financial_Func.py
+++ Financial_Func.py
@@ -16,7 +16,6 @@ import time
 import re
 import matplotlib.pyplot as plt
 
-#roles = pd.read_pickle('store_roles.pkl')
 
 '=============================================================='
 userid = ''
@@ -24,9 +23,8 @@ password = ''
 '=============================================================='
 
 driver = webdriver.Chrome(r"chromedriver.exe")
-
-driver.get('https://login.yahoo.com/')
 driver.maximize_window()
+driver.get('https://login.yahoo.com/')
 time.sleep(2)
 
 # Login
@@ -38,36 +36,38 @@ driver.find_element_by_xpath('//*[@class="password"]').send_keys(password)
 driver.find_element_by_xpath('//*[@id="login-signin"]').click()
 time.sleep(5)
 
-tickers = ['NOW']
+tickers = pd.read_csv('Comps.csv').iloc[:, 0].values.tolist()
 
-# Income Statement
-driver.get('https://finance.yahoo.com/quote/' + tickers[0] + '/financials?p=' + tickers[0])
-time.sleep(2)
-driver.find_element_by_xpath('//*[@class="P(0px) M(0px) C($linkColor) Bd(0px) O(n)"]').click()
-time.sleep(2)
-driver.find_element_by_xpath('//*[@class="Pos(r) smplTblTooltip C($linkColor) BdStart Bdc($seperatorColor) Pstart(10px) Mstart(10px)"]').click()
-time.sleep(2)
+for t in range(len(tickers)):
 
-# Balance Sheet
-driver.get('https://finance.yahoo.com/quote/' + tickers[0] + '/balance-sheet?p=' + tickers[0])
-time.sleep(2)
-driver.find_element_by_xpath('//*[@class="P(0px) M(0px) C($linkColor) Bd(0px) O(n)"]').click()
-time.sleep(2)
-driver.find_element_by_xpath('//*[@class="Pos(r) smplTblTooltip C($linkColor) BdStart Bdc($seperatorColor) Pstart(10px) Mstart(10px)"]').click()
-time.sleep(2)
+    # Income Statement
+    driver.get('https://finance.yahoo.com/quote/' + tickers[t] + '/financials?p=' + tickers[t])
+    time.sleep(3)
+    driver.find_element_by_xpath('//*[@class="P(0px) M(0px) C($linkColor) Bd(0px) O(n)"]').click()
+    time.sleep(3)
+    driver.find_element_by_xpath('//*[@class="Pos(r) smplTblTooltip C($linkColor) BdStart Bdc($seperatorColor) Pstart(10px) Mstart(10px)"]').click()
+    time.sleep(3)
 
-# Cash Flows
-driver.get('https://finance.yahoo.com/quote/' + tickers[0] + '/cash-flow?p=' + tickers[0])
-time.sleep(2)
-driver.find_element_by_xpath('//*[@class="P(0px) M(0px) C($linkColor) Bd(0px) O(n)"]').click()
-time.sleep(2)
-driver.find_element_by_xpath('//*[@class="Pos(r) smplTblTooltip C($linkColor) BdStart Bdc($seperatorColor) Pstart(10px) Mstart(10px)"]').click()
-time.sleep(2)
+    # Balance Sheet
+    driver.get('https://finance.yahoo.com/quote/' + tickers[t] + '/balance-sheet?p=' + tickers[t])
+    time.sleep(3)
+    driver.find_element_by_xpath('//*[@class="P(0px) M(0px) C($linkColor) Bd(0px) O(n)"]').click()
+    time.sleep(3)
+    driver.find_element_by_xpath('//*[@class="Pos(r) smplTblTooltip C($linkColor) BdStart Bdc($seperatorColor) Pstart(10px) Mstart(10px)"]').click()
+    time.sleep(3)
 
-# Stats
-driver.get('https://finance.yahoo.com/quote/' + tickers[0] + '/key-statistics?p=' + tickers[0])
-time.sleep(2)
-driver.find_element_by_xpath('//*[@class="Pos(r) smplTblTooltip C($linkColor) BdStart Bdc($seperatorColor) Pstart(10px) Mstart(10px)"]').click()
-time.sleep(2)
+    # Cash Flows
+    driver.get('https://finance.yahoo.com/quote/' + tickers[t] + '/cash-flow?p=' + tickers[t])
+    time.sleep(3)
+    driver.find_element_by_xpath('//*[@class="P(0px) M(0px) C($linkColor) Bd(0px) O(n)"]').click()
+    time.sleep(3)
+    driver.find_element_by_xpath('//*[@class="Pos(r) smplTblTooltip C($linkColor) BdStart Bdc($seperatorColor) Pstart(10px) Mstart(10px)"]').click()
+    time.sleep(3)
+
+    # Stats
+    driver.get('https://finance.yahoo.com/quote/' + tickers[t] + '/key-statistics?p=' + tickers[t])
+    time.sleep(2)
+    driver.find_element_by_xpath('//*[@class="Pos(r) smplTblTooltip C($linkColor) BdStart Bdc($seperatorColor) Pstart(10px) Mstart(10px)"]').click()
+    time.sleep(2)
 
 driver.quit()
